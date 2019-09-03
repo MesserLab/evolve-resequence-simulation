b98f41a1533852e011f3be5005edafbcecf0c67d
Runyang Lou <rl683@cbsulogin3.biohpc.cornell.edu>
2019-09-02 22:25:23 -0400

    added transformed D

--- Figures/figure_2.R
+++ Figures/figure_2.R
@@ -16,8 +16,7 @@ if (Type == "Direction"){
   Direction <- c("Minus")
 } else {
   NExpRep <- 1
-  Directions <- c("Plus"
-                  , "Minus"
+  Directions <- c("Minus"
   )
 }
 WindowSize <- 0
@@ -28,307 +27,17 @@ OutPath="/fs/cbsubscb10/storage/rl683/TemporalScan/"
 SampleSize=50 # make sure that the simulation had SampleSize LARGER or EQUAL to this
 NSimRep=100 # make sure that the simulation had NSimRep LARGER or EQUAL to this
 
-library(ggplot2)
+library(tidyverse)
 
-if (Type %in% c("QTL_architecture", "Experimental_design")){ # define filenames, plot title, and plot labels in the begining of the following loop 
-  for (i in c(11)){
-    titles <- c("Number_of_SNPs"
-                , "Number_of_QTLs"
-                , "Starting_frequency_10_QTLs"
-                , "Starting_frequency_100_QTLs"
-                , "Effect_size_distribution_10_QTLs"
-                , "Effect_size_distribution_100_QTLs"
-                , "Dominance_10_QTLs"
-                , "Dominance_100_QTLs"
-                , "Position_10_QTLs"
-                , "Position_100_QTLs"
-                , "Epistasis_scenarios"
-                , "ESDistxNGenxSelectedSize"
-                , "Number_of_generations_100_QTLs"
-                , "Number_of_generations_10_QTLs"
-    )
-    
-    filenames <-   list(c("NQTL10_NGen5", "NQTL10_FewerSNPs_NGen5")
-                        , c("NQTL10_NGen5", "NQTL100_NGen5", "NQTL2_NGen5", "NQTL20_NGen5", "NQTL200_NGen5")
-                        , c("NQTL10_NGen5", "NQTL10_FreqLow5_NGen5", "NQTL10_FreqHigh5_NGen5")
-                        , c("NQTL100_NGen5", "NQTL100_FreqLow5_NGen5", "NQTL100_FreqHigh5_NGen5")
-                        , c("NQTL10_NGen5", "NQTL10_ESDistE_NGen5")
-                        , c("NQTL100_NGen5", "NQTL100_ESDistE_NGen5")
-                        , c("NQTL10_NGen5",  "NQTL10_D0_NGen5", "NQTL10_D1_NGen5")
-                        , c("NQTL100_NGen5", "NQTL100_D0_NGen5", "NQTL100_D1_NGen5")
-                        , c("NQTL10_NGen5", "NQTL10_Clustered_NGen5")
-                        , c("NQTL100_NGen5", "NQTL100_Clustered_NGen5")
-                        , c("NQTL10_NGen5", "NQTL10_EpiSce9_NGen5", "NQTL10_EpiSce4_NGen5", "NQTL10_EpiSce8_NGen5", "NQTL10_EpiSce1_NGen5", "NQTL10_EpiSce5_NGen5", "NQTL10_EpiSce10_NGen5", "NQTL10_EpiSce7_NGen5", "NQTL10_EpiSce11_NGen5")
-                        , c("NQTL100", "NQTL100_ESDistE", "NQTL100_NGen20SelectedSize200", "NQTL100_NGen20SelectedSize200ESDistE")
-                        , c("NQTL100", "NQTL100_NGen5")
-                        , c("NQTL10", "NQTL10_NGen5")
-    )
-    
-    labels <- list(c("~14000 SNPs \n(Standard)", "~1400 SNPs")
-                   , c("10 QTL (Standard)", "100 QTL (Standard)", "2 QTL", "20 QTL",  "200 QTL")
-                   , c("Random (Standard)", "Lower than 5%", "Higher than 5%")
-                   , c("Random (Standard)", "Lower than 5%", "Higher than 5%")
-                   , c("Equal (Standard)", "Exponential")
-                   , c("Equal (Standard)", "Exponential")
-                   , c("Mutant is codominant \n(Standard)", "Mutant is recessive", "Mutant is dominant")
-                   , c("Mutant is codominant \n(Standard)", "Mutant is recessive", "Mutant is dominant")
-                   , c("Random (Standard)", "Clustered")
-                   , c("Random (Standard)", "Clustered")
-                   , c("No epistatisis (Standard)", "Synergistic, weak", "Synergistic, strong", "Antaganistic, weak", "Antaganistic, strong", "Sign, weak",  "Sign, strong", "Reciprocal sign, weak", "Reciprocal sign, strong")
-                   , c("Fixed, 10 Generations, 10% Selected (Basline)", "Exponential, 10 Generations, 10% Selected", "Fixed, 20 Generations, 20% Selected", "Exponential, 20 Generations, 20% Selected")
-                   , c("10 Generations (Standard)", "5 Generations")
-                   , c("10 Generations (Standard)", "5 Generations")
-    )
-    
-    title <- titles[i]
-    filenames <- filenames[[i]]
-    labels <- labels[[i]]
-    
-    
-    n_files <- length(filenames)
-    for (Direction in Directions) {
-      for (filename in filenames){
-        for (k in 1:NSimRep){
-          if (Direction == "Plus") {
-            DirectionName <- "SingleDirection"
-          } else if (Direction == "Minus"){
-            DirectionName <- "OppositeDirections"
-          }
-          setwd(paste0(OutPath, "Simulations/", filename, "/SimRep", k, "/ExpRep", Direction, NExpRep))
-          ROC <- read.table("ROC_TwoTimepoints.txt", sep = ",",  header = T, stringsAsFactors = F)
-          if(dim(ROC)[2]==6){
-            ROC <- cbind(ROC, ROC[,2])
-          }
-          colnames(ROC)<- c("Proportion_of_genetic_variance_in_gen_1", 
-                            "Proportion_of_QTL_detected_weighted", 
-                            "False_positive_rate",
-                            "Window_size",
-                            "Computational_method",
-                            "Numbers_of_timepoints_sampled",
-                            "Proportion_of_QTL_detected_unweighted")
-          ROC <- ROC[which(ROC$Window_size==WindowSize & ROC$Computational_method==ComputationalMethod),]
-          if (k == 1){
-            ROC_final <- ROC[,c(1:3,7)]
-          } else {
-            ROC_final <- ROC[,c(1:3,7)]+ROC_final
-          }
-        }
-        ROC_final <- ROC_final/NSimRep
-        ROC_final <- cbind(ROC_final, filename)
-        color <- which(filenames == filename)
-        ROC_final <- cbind(ROC_final, color) 
-        if (!exists("ROC_QTL_archetecture_pooled")){
-          ROC_QTL_archetecture_pooled <- ROC_final
-        } else {
-          ROC_QTL_archetecture_pooled <- rbind(ROC_QTL_archetecture_pooled, ROC_final)
-        } 
-      }
-      library(ggplot2)
-      
-      p1 <- ggplot(ROC_QTL_archetecture_pooled, aes(False_positive_rate, Proportion_of_genetic_variance_in_gen_1, color = factor(color, labels = labels))) + 
-        geom_line(size = 2, alpha = 0.8, linetype="dotted") + 
-        geom_line(data=ROC_QTL_archetecture_pooled[which(ROC_QTL_archetecture_pooled[,1]>0 | ROC_QTL_archetecture_pooled[,3]>0),], aes(False_positive_rate, Proportion_of_genetic_variance_in_gen_1, color = factor(color, labels = labels)), size = 2, alpha = 1) + 
-        scale_x_continuous(breaks = seq(0,0.1,0.02)) + 
-        scale_y_continuous(limits = c(0,1)) + 
-        coord_cartesian(xlim = c(0,0.1)) +
-        xlab("False positive rate") +
-        ylab("Proportion of genetic variance detected") +
-        theme_bw() +
-        theme(legend.position="none") +
-        theme(axis.title = element_text(size = 30)) +
-        theme(axis.text=element_text(size=30)) +
-        theme(text = element_text(size=30)) +
-        ggtitle(NULL)
-      p2 <- ggplot(ROC_QTL_archetecture_pooled, aes(False_positive_rate,Proportion_of_QTL_detected_weighted, color = factor(color, labels = labels))) + 
-        geom_line(size = 2, alpha = 0.8, linetype="dotted") + 
-        geom_line(data=ROC_QTL_archetecture_pooled[which(ROC_QTL_archetecture_pooled[,2]>0 | ROC_QTL_archetecture_pooled[,3]>0),], aes(False_positive_rate, Proportion_of_QTL_detected_weighted, color = factor(color, labels = labels)), size = 2, alpha = 1) + 
-        scale_x_continuous(breaks = seq(0,0.1,0.02)) + 
-        scale_y_continuous(limits = c(0,1)) + 
-        coord_cartesian(xlim = c(0,0.1)) +
-        xlab("False positive rate") +
-        ylab("Proportion of QTL detected weighted by effect size") +
-        theme_bw() +
-        theme(legend.position="none") +
-        theme(axis.title = element_text(size = 30)) +
-        theme(axis.text=element_text(size=30)) +
-        theme(text = element_text(size=30)) +
-        ggtitle(NULL) 
-      p3 <- ggplot(ROC_QTL_archetecture_pooled, aes(False_positive_rate,Proportion_of_QTL_detected_unweighted, color = factor(color, labels = labels))) +
-        geom_line(size = 2, alpha = 0.8, linetype="dotted") +
-        geom_line(data=ROC_QTL_archetecture_pooled[which(ROC_QTL_archetecture_pooled[,2]>0 | ROC_QTL_archetecture_pooled[,3]>0),], aes(False_positive_rate, Proportion_of_QTL_detected_unweighted, color = factor(color, labels = labels)), size = 2, alpha = 1) +
-        scale_x_continuous(breaks = seq(0,0.1,0.02)) +
-        scale_y_continuous(limits = c(0,1)) +
-        coord_cartesian(xlim = c(0,0.1)) +
-        xlab("False positive rate") +
-        ylab("Proportion of QTL detected") +
-        theme_bw() +
-        theme(legend.position="none") +
-        theme(axis.title = element_text(size = 30)) +
-        theme(axis.text=element_text(size=30)) +
-        theme(text = element_text(size=30)) +
-        ggtitle(NULL)
-      
-      p4 <- p1 +
-        theme(legend.position = c(0.75, 0.2)) +
-        theme(legend.text=element_text(size=30)) +
-        theme(legend.background = element_rect(fill = "transparent")) +
-        guides(color=guide_legend(title="")) +
-        theme(axis.title = element_text(size = 30)) +
-        theme(axis.text=element_text(size=30))
-      p5 <- p2 + 
-        theme(legend.position = c(0.75, 0.2)) +
-        theme(legend.text=element_text(size=30)) +
-        theme(legend.background = element_rect(fill = "transparent")) +
-        guides(color=guide_legend(title="")) +
-        theme(axis.title = element_text(size = 30)) +
-        theme(axis.text=element_text(size=30))
-      p6 <- p3 +
-        theme(legend.position = c(0.75, 0.2)) +
-        theme(legend.text=element_text(size=30)) +
-        theme(legend.background = element_rect(fill = "transparent")) +
-        guides(color=guide_legend(title="")) +
-        theme(axis.title = element_text(size = 30)) +
-        theme(axis.text=element_text(size=30))
-      
-      setwd(paste0(OutPath, "Figures/ROC/", title, "/"))
-      
-      png(paste0(title[1], "_", DirectionName, "_Zoomed_NGen5_Variance.png"), width = 800, height = 750, units = "px", pointsize = 20)
-      print(p1)
-      dev.off()
-      png(paste0(title[1], "_", DirectionName, "_Zoomed_NGen5_Weighted.png"), width = 800, height = 750, units = "px", pointsize = 20)
-      print(p2)
-      dev.off()
-      png(paste0(title[1], "_", DirectionName, "_Zoomed_NGen5_Unweighted.png"), width = 800, height = 750, units = "px", pointsize = 20)
-      print(p3)
-      dev.off()
-      png(paste0(title[1], "_", DirectionName, "_Zoomed_NGen5_Variance_Legend.png"), width = 800, height = 750, units = "px", pointsize = 20)
-      print(p4)
-      dev.off()
-      png(paste0(title[1], "_", DirectionName, "_Zoomed_NGen5_Weighted_Legend.png"), width = 800, height = 750, units = "px", pointsize = 20)
-      print(p5)
-      dev.off()
-      png(paste0(title[1], "_", DirectionName, "_Zoomed_NGen5_Unweighted_Legend.png"), width = 800, height = 750, units = "px", pointsize = 20)
-      print(p6)
-      dev.off()
-      
-      rm(ROC_QTL_archetecture_pooled)
-    }
-  }
-} else if (Type %in% c("Replication", "Direction")){
-  if (Type == "Replication"){
-    NsExpRep <- paste0(Direction, NsExpRep)
-  } else{
-    NsExpRep <- paste0(Directions, NExpRep)
-  }
-  for (n_exp_rep in NsExpRep){
-    for (sim_rep_id in 1:NSimRep){
-      setwd(paste0(OutPath, "Simulations/", filename, "/SimRep", sim_rep_id, "/", "ExpRep", n_exp_rep, "/")) ## set directory to experimental replication
-      ROC <- read.table("ROC_TwoTimepoints.txt", sep = ",",  header = T, stringsAsFactors = F)
-      colnames(ROC)<- c("Proportion_of_genetic_variance_in_gen_1", 
-                        "Proportion_of_QTL_detected_weighted", 
-                        "False_positive_rate",
-                        "Window_size",
-                        "Computational_method",
-                        "Numbers_of_timepoints_sampled")
-      ROC <- ROC[which(ROC$Window_size==WindowSize & ROC$Computational_method==ComputationalMethod),]
-      if(sim_rep_id==1){
-        ROC_temp <- ROC[,1:3]
-      } else {
-        ROC_temp <- ((sim_rep_id-1)*ROC_temp + ROC[,1:3])/sim_rep_id
-      }
-    }
-    color <- which(NsExpRep == n_exp_rep)
-    ROC_temp <- cbind(ROC_temp, as.character(n_exp_rep), color)
-    if(n_exp_rep==NsExpRep[1]){
-      ROC_final <- ROC_temp
-    } else {
-      ROC_final <- rbind(ROC_final, ROC_temp)
-    }
-  }
-  
-  p1 <- ggplot(ROC_final, aes(False_positive_rate,Proportion_of_genetic_variance_in_gen_1, color = factor(color, labels = NsExpRep))) + 
-    #geom_point(size = 3, alpha = 0.8, shape=1) + 
-    geom_line(size = 2, alpha = 0.8, linetype="dotted") + 
-    #geom_point(data=ROC_final[which(ROC_final[,1]>0 | ROC_final[,3]>0),], aes(False_positive_rate, Proportion_of_genetic_variance_in_gen_1, color = factor(color, labels = NsExpRep)), size = 3, alpha = 0.8) + 
-    geom_line(data=ROC_final[which(ROC_final[,1]>0 | ROC_final[,3]>0),], aes(False_positive_rate, Proportion_of_genetic_variance_in_gen_1, color = factor(color, labels = NsExpRep)), size = 2, alpha = 1) + 
-    scale_x_continuous(limits = c(0,1), breaks = seq(0,1,0.2)) + 
-    xlab("False positive rate") +
-    ylab("Proportion of genetic variance detected") +
-    theme(legend.position="none") +
-    theme(axis.title = element_text(size = 30)) +
-    theme(axis.text=element_text(size=30)) +
-    theme(text = element_text(size=30)) +
-    ggtitle(NULL)
-  
-  
-  p2 <- ggplot(ROC_final, aes(False_positive_rate,Proportion_of_genetic_variance_in_gen_1, color = factor(color, labels = NsExpRep))) + 
-    #geom_point(size = 3, alpha = 0.8, shape=1) + 
-    geom_line(size = 2, alpha = 0.8, linetype="dotted") + 
-    #geom_point(data=ROC_final[which(ROC_final[,1]>0 | ROC_final[,3]>0),], aes(False_positive_rate, Proportion_of_genetic_variance_in_gen_1, color = factor(color, labels = NsExpRep)), size = 3, alpha = 0.8) + 
-    geom_line(data=ROC_final[which(ROC_final[,1]>0 | ROC_final[,3]>0),], aes(False_positive_rate, Proportion_of_genetic_variance_in_gen_1, color = factor(color, labels = NsExpRep)), size = 2, alpha = 1) + 
-    scale_x_continuous(limits = c(0,0.105), breaks = seq(0,0.1,0.02)) + 
-    scale_y_continuous(limits = c(0,0.7)) + 
-    xlab("False positive rate") +
-    ylab("Proportion of genetic variance detected") +
-    theme(legend.position="none") +
-    theme(axis.title = element_text(size = 30)) +
-    theme(axis.text=element_text(size=30)) +
-    theme(text = element_text(size=30)) +
-    ggtitle(NULL)
-  
-  p3 <- ggplot(ROC_final, aes(False_positive_rate,Proportion_of_QTL_detected_weighted, color = factor(color, labels = NsExpRep))) + 
-    #geom_point(size = 3, alpha = 0.8, shape=1) + 
-    geom_line(size = 2, alpha = 0.8, linetype="dotted") + 
-    #geom_point(data=ROC_final[which(ROC_final[,2]>0 | ROC_final[,3]>0),], aes(False_positive_rate, Proportion_of_QTL_detected_weighted, color = factor(color, labels = NsExpRep)), size = 3, alpha = 0.8) + 
-    geom_line(data=ROC_final[which(ROC_final[,2]>0 | ROC_final[,3]>0),], aes(False_positive_rate, Proportion_of_QTL_detected_weighted, color = factor(color, labels = NsExpRep)), size = 2, alpha = 1) + 
-    scale_x_continuous(limits = c(0,1), breaks = seq(0,1,0.2)) + 
-    xlab("False positive rate") +
-    ylab("Proportion of QTL detected weighted by effect size") +
-    guides(color=guide_legend(title="")) +
-    theme(legend.position = c(0.65, 0.2)) +
-    theme(axis.title = element_text(size = 30)) +
-    theme(axis.text=element_text(size=30)) +
-    theme(legend.text=element_text(size=30)) +
-    theme(legend.background = element_rect(fill = "transparent")) +
-    theme(text = element_text(size=30)) +
-    ggtitle(NULL)
-  
-  
-  p4 <- ggplot(ROC_final, aes(False_positive_rate,Proportion_of_QTL_detected_weighted, color = factor(color, labels = NsExpRep))) + 
-    #geom_point(size = 3, alpha = 0.8, shape=1) + 
-    geom_line(size = 2, alpha = 0.8, linetype="dotted") + 
-    #geom_point(data=ROC_final[which(ROC_final[,2]>0 | ROC_final[,3]>0),], aes(False_positive_rate, Proportion_of_QTL_detected_weighted, color = factor(color, labels = NsExpRep)), size = 3, alpha = 0.8) + 
-    geom_line(data=ROC_final[which(ROC_final[,2]>0 | ROC_final[,3]>0),], aes(False_positive_rate, Proportion_of_QTL_detected_weighted, color = factor(color, labels = NsExpRep)), size = 2, alpha = 1) + 
-    scale_x_continuous(breaks = seq(0,0.1,0.02)) + 
-    scale_y_continuous(limits = c(0,0.5)) + 
-    xlab("False positive rate") +
-    ylab("Proportion of QTL detected weighted by effect size") +
-    guides(color=guide_legend(title="")) +
-    theme(legend.position = c(0.65, 0.2)) +
-    theme(axis.title = element_text(size = 30)) +
-    theme(axis.text=element_text(size=30)) +
-    theme(legend.text=element_text(size=30)) +
-    theme(legend.background = element_rect(fill = "transparent")) +
-    theme(text = element_text(size=30)) +
-    ggtitle(NULL)
-  
-  setwd(paste0(OutPath, "Figures/ROC/", Type, "/"))
-  
-  png(paste0(title, "_Original.png"), width = 1500, height = 760, units = "px", pointsize = 20)
-  multiplot(p1, p3, layout = matrix(c(1,2), ncol=2))
-  dev.off()
-  
-  png(paste0(title, "_Zoomed.png"), width = 1500, height = 760, units = "px", pointsize = 20)
-  multiplot(p2, p4, layout = matrix(c(1,2), ncol=2))
-  dev.off()
-  
-} else if (Type %in% c("ComputationalMethod")){
+if (Type %in% c("ComputationalMethod")){
   for (Direction in Directions){
     for (sim_rep_id in 1:NSimRep){
       setwd(paste0(OutPath, "Simulations/", filename, "/SimRep", sim_rep_id, "/", "ExpRep", Direction, NExpRep, "/")) ## set directory to experimental replication
       ROC <- read.table("ROC_TwoTimepoints_NGen5.txt", sep = ",",  header = T, stringsAsFactors = F)
       temp <- read.table("ROC_AllTimepoints_NGen5.txt", sep = ",",  header = T, stringsAsFactors = F)
-      ROC <- rbind(ROC, temp)
+      temp2 <- read.table("ROC_TwoTimepoints_NGen5_TransformedD.txt", sep = ",",  header = T, stringsAsFactors = F) %>%
+        mutate(Computational_method = "TransformedD")
+      ROC <- rbind(ROC, temp, temp2)
       ROC[,5]<-paste0(ROC[,5], "_", ROC[,6])
       ROC <- ROC[,-6]
       colnames(ROC)<- c("Proportion_of_genetic_variance_in_gen_1", 
@@ -342,11 +51,14 @@ if (Type %in% c("QTL_architecture", "Experimental_design")){ # define filenames,
         ROC_temp <- ((sim_rep_id-1)*ROC_temp + ROC[,1:3])/sim_rep_id
       }
     }
+    ROC_temp<-ROC_temp[c(1:100, 501:600, 101:200, 301:400, 201:300, 401:500),]
+    ROC<-ROC[c(1:100, 501:600, 101:200, 301:400, 201:300, 401:500),]
     color <- sapply(ROC[,5], function(x) {which(unique(ROC[,5])==x)})
-    labels <- c("D (2 Timepoints)", 
+    labels <- c("D (2 Timepoints)",
+                "Transformed D (2 Timepoints)", 
                 "WFABC (2 Timepoints)", 
-                "ApproxWF (2 Timepoints)",
                 "WFABC (5 Timepoints)",
+                "ApproxWF (2 Timepoints)",
                 "ApproxWF (5 Timepoints)")
     Computational_method <-ROC[,5]
     ROC_final <- cbind(ROC_temp, Computational_method, color)
@@ -393,7 +105,7 @@ if (Type %in% c("QTL_architecture", "Experimental_design")){ # define filenames,
       theme(axis.text=element_text(size=30))
     
     p4 <- p2 +
-      theme(legend.position = c(0.7, 0.18)) +
+      theme(legend.position = c(0.65, 0.78)) +
       theme(legend.text=element_text(size=30)) +
       theme(legend.background = element_rect(fill = "transparent", color="black")) +
       theme(legend.title = element_blank()) +
@@ -424,3 +136,4 @@ if (Type %in% c("QTL_architecture", "Experimental_design")){ # define filenames,
     
   }
 }
+
